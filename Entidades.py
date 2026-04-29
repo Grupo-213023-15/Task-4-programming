8da07fbb2e7cd3e1cd2467137a740763d8592c22
Jason Day <jasondaymorenogutierrez@gmail.com>
2026-04-29 13:45:29 -0500

    Estructura base: Entidades y Logger

---
+++ Entidades.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+# [21] clase abstracta para entidades generales
+class entidadsistema(ABC):
+    def__init__(self, id_entidad):
+        self.id_entidad = id_entidad
+    
+    @abstractmethod
+    def mostrar_detalle(self):
+        pass
+    
+# [22] clase cliente con encapsulación de datos personales
+class cliente(entidadsistema):
+    def__init__(self, id_entidad, nombre, correo):
+        super().__init__(id_entidad)
+        self.__nombre = nombre #atributo privado
+        self.__correo = self.__validar_correo(correo)
+        
+    def __validar_correo(self, correo):
+        if "@" not in correo:
+            raise ValueError("correo inválido")
+    return correo
+
+    def mostrar_detalle(self):
+        return f"cliente: {self.__nombre} (id: {self.id__entidad})"
---
+++ logger_config.py
@@ -0,0 +1,12 @@
+import logging
+
+# configuración para que los errores se guarden en 'errores.log'
+logging.basicConfig(
+    filename='errores.log',
+    level=logging.error,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def registrar_error(mensaje):
+    logging.error(mensaje)
+    
